b8c77a87e19221105199c60ffbffe55bd1205357
IvanMonroy <inter250296@hotmail.com>
2020-02-22 15:36:24 -0500

    ...

--- db/migrate/20200222184346_change_index_new_id.rb
+++ db/migrate/20200222184346_change_index_new_id.rb
@@ -1,6 +1,6 @@
 class ChangeIndexNewId < ActiveRecord::Migration[5.2]
   def up
-    rename_column :images_news, :index_new_id, :index_news_id
+    rename_column :images_news, :index_news_id, :index_new_id
   end
   def down
     rename_column :images_news, :index_news_id, :index_new_id
