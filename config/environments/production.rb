e0035953c272c870305eff35f7250f34184da3fe
IvanMonroy <inter250296@hotmail.com>
2020-04-10 19:38:37 -0500

    prueba

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,8 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,6 +17,7 @@ Rails.application.configure do
   config.public_file_server.headers = {
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
@@ -44,7 +45,6 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
